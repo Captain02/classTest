909cdf199da9edba43b186f56e6e9ad6a8817d7b
Captain02 <1076453997@qq.com>
2022-02-10 11:46:47 +0800

    微课堂
    微课堂

--- SourceV7/jzysoft-admin/src/main/resources/static/js/cClassStu/cClassStu.js
+++ SourceV7/jzysoft-admin/src/main/resources/static/js/cClassStu/cClassStu.js
@@ -3,13 +3,13 @@ $(function () {
         url: baseURL + 'cClassStu/cClassStu/list',
         datatype: "json",
         colModel: [
-                                                {label: '${column.columnComment}',name: 'id',index: 'id',width: 0,key: true,hidden: true},
-                                                                {label: '班级主键',name: 'classnum',index: 'classnum',width: 80}, 
-                                                                {label: '学生主键',name: 'studnum',index: 'studnum',width: 80}
-                            
+            {label: '${column.columnComment}', name: 'id', index: 'id', width: 0, key: true, hidden: true},
+            {label: '班级主键', name: 'classnum', index: 'classnum', width: 80},
+            {label: '学生主键', name: 'studnum', index: 'studnum', width: 80}
+
         ],
         viewrecords: true,
-         height: "100%",
+        height: "100%",
         rowNum: 10,
         rowList: [10, 30, 50],
         rownumbers: true,
@@ -47,157 +47,157 @@ var vm = new Vue({
 
     data: {
         q: {
-                classnum:null,
-                studnum:null,
-    },
-showList: true,
-    title: null,
-    index: null,
-    edit: {
-            id:null,
-            classnum:null,
-            studnum:null,
-    },
-dropdown: [
-    {
-        id: null,
-        name: null,
-    }
-],
-    dropdown1: [
-    {
-        id: null,
-        name: null,
-    }
-]
-},
-created: function () {
-    this.bindsearchdropdown();
-    this.binddropdown();
-},
-methods: {
-    binddropdown:function(){
-        //字典表下拉框
-        this.dropdown1 = BindDropDownControlsdy('sys_dict_data', 'dict_', 'dict_label', '起重机类型管理')
-        // 普通下拉框
-        this.dropdown = BindDropDownControls('customer', 'customer_id', 'username');
-    },
-    // 搜索下拉
-    bindsearchdropdown: function () {
-        BindDropDownControl('selectuserid', 'customer', 'customer_id', 'username')
-    },
-    // 添加页面
-    addPage: function () {
-        vm.reload();
-        vm.index = layer.open({
-            type: 1,
-            area: ['95%', '95%'],
-            content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
-             btn: ['确定', '取消'],
-             yes: function () {
-                 vm.saveOrUpdate();
-             },
-             btn2: function () {
-                 vm.reload();
-             }
-        });
-    },
-    <!--修改页面-->
-    editPage: function (x) {
-        var id = getSelectedRow();
-        if (id == null) {
-            return;
-        }
-        $.ajax({
-            url: baseURL + 'cClassStu/cClassStu/findByid',
-            type: "GET",
-            data: {
-        id: id,
-            },
-            success: function (result) {
-                                    vm.edit.id= result.data.id;
-                                    vm.edit.classnum= result.data.classnum;
-                                    vm.edit.studnum= result.data.studnum;
-                            }
-        })
-        vm.index = layer.open({
-            type: 1,
-            area: ['95%', '95%'],
-            content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
-             btn: ['确定', '取消'],
-             yes: function (id) {
-                 vm.saveOrUpdate(id);
-             },
-             btn2: function () {
-                 vm.reload();
-             }
-        });
-    },
-    <!--新增修改-->
-    saveOrUpdate: function (id) {
-        var url = id == null ? "cClassStu/cClassStu/adddata" : "cClassStu/cClassStu/updatedata";
-        $.ajax({
-            url: baseURL + url,
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(vm.edit),
-            success: function (result) {
-                vm.reload();
-                layer.close(vm.index)
-            },
-            btn2: function () {
-                vm.reload();
+            classnum: null,
+            studnum: null,
+        },
+        showList: true,
+        title: null,
+        index: null,
+        edit: {
+            id: null,
+            classnum: null,
+            studnum: null,
+        },
+        dropdown: [
+            {
+                id: null,
+                name: null,
             }
-        })
+        ],
+        dropdown1: [
+            {
+                id: null,
+                name: null,
+            }
+        ]
     },
-    // 搜索
-    query: function () {
-        var page = $("#jqGrid").jqGrid('getGridParam', 'page');
-        $("#jqGrid").jqGrid('setGridParam', {
-            postData: {
-                                                                                                'classnum':vm.q.classnum,
-                                                                                'studnum':vm.q.studnum,
-                                                },
-            page: page
-        }).trigger("reloadGrid");
+    created: function () {
+        this.bindsearchdropdown();
+        this.binddropdown();
     },
-    // 删除
-    del: function (event) {
-        var ids = getSelectedRows();
-        if (ids == null) {
-            return;
-        }
-        var r=layer.confirm('您确定要删除该用户吗?',{btn: ['确定', '取消'],title:"提示"}, function() {
-            if (r) {
-            $.ajax({
-                type: "POST",
-                url: baseURL + "/cClassStu/cClassStu/del",
-                contentType: "application/json",
-                data: JSON.stringify(ids),
-                success: function (r) {
-                    if (r.code = 1) {
-                        vm.reload();
-                    } else {
-                        alert("删除失败")
-                    }
-
+    methods: {
+        binddropdown: function () {
+            //字典表下拉框
+            this.dropdown1 = BindDropDownControlsdy('sys_dict_data', 'dict_', 'dict_label', '起重机类型管理')
+            // 普通下拉框
+            this.dropdown = BindDropDownControls('customer', 'customer_id', 'username');
+        },
+        // 搜索下拉
+        bindsearchdropdown: function () {
+            BindDropDownControl('selectuserid', 'customer', 'customer_id', 'username')
+        },
+        // 添加页面
+        addPage: function () {
+            vm.reload();
+            vm.index = layer.open({
+                type: 1,
+                area: ['95%', '95%'],
+                content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
+                btn: ['确定', '取消'],
+                yes: function () {
+                    vm.saveOrUpdate();
+                },
+                btn2: function () {
+                    vm.reload();
                 }
             });
+        },
+        <!--修改页面-->
+        editPage: function (x) {
+            var id = getSelectedRow();
+            if (id == null) {
+                return;
             }
-        });
-    },
-    // 重置
-    reset:function () {
-                                                        vm.q.classnum=null;
-                                                vm.q.studnum=null;
-                            $("#gen-form")[0].reset();
-    },
-    reload: function () {
-                                                        vm.edit.classnum=null;
-                                                vm.edit.studnum=null;
-                            $("#editLayer")[0].reset();
-        layer.closeAll();
-        $("#jqGrid").trigger("reloadGrid");
+            $.ajax({
+                url: baseURL + 'cClassStu/cClassStu/findByid',
+                type: "GET",
+                data: {
+                    id: id,
+                },
+                success: function (result) {
+                    vm.edit.id = result.data.id;
+                    vm.edit.classnum = result.data.classnum;
+                    vm.edit.studnum = result.data.studnum;
+                }
+            })
+            vm.index = layer.open({
+                type: 1,
+                area: ['95%', '95%'],
+                content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
+                btn: ['确定', '取消'],
+                yes: function (id) {
+                    vm.saveOrUpdate(id);
+                },
+                btn2: function () {
+                    vm.reload();
+                }
+            });
+        },
+        <!--新增修改-->
+        saveOrUpdate: function (id) {
+            var url = id == null ? "cClassStu/cClassStu/adddata" : "cClassStu/cClassStu/updatedata";
+            $.ajax({
+                url: baseURL + url,
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(vm.edit),
+                success: function (result) {
+                    vm.reload();
+                    layer.close(vm.index)
+                },
+                btn2: function () {
+                    vm.reload();
+                }
+            })
+        },
+        // 搜索
+        query: function () {
+            var page = $("#jqGrid").jqGrid('getGridParam', 'page');
+            $("#jqGrid").jqGrid('setGridParam', {
+                postData: {
+                    'classnum': vm.q.classnum,
+                    'studnum': vm.q.studnum,
+                },
+                page: page
+            }).trigger("reloadGrid");
+        },
+        // 删除
+        del: function (event) {
+            var ids = getSelectedRows();
+            if (ids == null) {
+                return;
+            }
+            var r = layer.confirm('您确定要删除该用户吗?', {btn: ['确定', '取消'], title: "提示"}, function () {
+                if (r) {
+                    $.ajax({
+                        type: "POST",
+                        url: baseURL + "/cClassStu/cClassStu/del",
+                        contentType: "application/json",
+                        data: JSON.stringify(ids),
+                        success: function (r) {
+                            if (r.code = 1) {
+                                vm.reload();
+                            } else {
+                                alert("删除失败")
+                            }
+
+                        }
+                    });
+                }
+            });
+        },
+        // 重置
+        reset: function () {
+            vm.q.classnum = null;
+            vm.q.studnum = null;
+            $("#gen-form")[0].reset();
+        },
+        reload: function () {
+            vm.edit.classnum = null;
+            vm.edit.studnum = null;
+            $("#editLayer")[0].reset();
+            layer.closeAll();
+            $("#jqGrid").trigger("reloadGrid");
+        }
     }
-}
 });
--- SourceV7/jzysoft-admin/src/main/resources/static/js/cMcurr/cMcurr.js
+++ SourceV7/jzysoft-admin/src/main/resources/static/js/cMcurr/cMcurr.js
@@ -3,19 +3,20 @@ $(function () {
         url: baseURL + 'cMcurr/cMcurr/list',
         datatype: "json",
         colModel: [
-                                                {label: '${column.columnComment}',name: 'id',index: 'id',width: 0,key: true,hidden: true},
-                                                                {label: '微课堂名称',name: 'mname',index: 'mname',width: 80}, 
-                                                                {label: '任教老师',name: 'teacherid',index: 'teacherid',width: 80}, 
-                                                                {label: '创建时间',name: 'createtime',index: 'createtime',width: 80}, 
-                                                                {label: '${column.columnComment}',name: 'isexamine',index: 'isexamine',width: 80}, 
-                                                                {label: '父课程',name: 'parentid',index: 'parentid',width: 80}, 
-                                                                {label: '备注1',name: 'remarks1',index: 'remarks1',width: 80}, 
-                                                                {label: '备注2',name: 'remarks2',index: 'remarks2',width: 80}, 
-                                                                {label: '备注3',name: 'remarks3',index: 'remarks3',width: 80}
-                            
+            {label: '${column.columnComment}', name: 'id', index: 'id', width: 0, key: true, hidden: true},
+            {label: '微课堂名称', name: 'mname', index: 'mname', width: 80},
+            {label: '任教老师', name: 'teacherid', index: 'teacherid', width: 80},
+            {label: '创建时间', name: 'createtime', index: 'createtime', width: 80},
+            // {label: '是否审核通过', name: 'isexamine', index: 'isexamine', width: 80},
+            {label: '父课程', name: 'parentid', index: 'parentid', width: 80},
+            {label: '是否审核通过', name: 'isexaminestr', index: 'isexaminestr', width: 80},
+            {label: '备注1', name: 'remarks1', index: 'remarks1', width: 80},
+            {label: '备注2', name: 'remarks2', index: 'remarks2', width: 80},
+            {label: '备注3', name: 'remarks3', index: 'remarks3', width: 80}
+
         ],
         viewrecords: true,
-         height: "100%",
+        height: "100%",
         rowNum: 10,
         rowList: [10, 30, 50],
         rownumbers: true,
@@ -53,193 +54,193 @@ var vm = new Vue({
 
     data: {
         q: {
-                mname:null,
-                teacherid:null,
-                createtime:null,
-                isexamine:null,
-                parentid:null,
-                remarks1:null,
-                remarks2:null,
-                remarks3:null,
-    },
-showList: true,
-    title: null,
-    index: null,
-    edit: {
-            id:null,
-            mname:null,
-            teacherid:null,
-            createtime:null,
-            isexamine:null,
-            parentid:null,
-            remarks1:null,
-            remarks2:null,
-            remarks3:null,
-    },
-dropdown: [
-    {
-        id: null,
-        name: null,
-    }
-],
-    dropdown1: [
-    {
-        id: null,
-        name: null,
-    }
-]
-},
-created: function () {
-    this.bindsearchdropdown();
-    this.binddropdown();
-},
-methods: {
-    binddropdown:function(){
-        //字典表下拉框
-        this.dropdown1 = BindDropDownControlsdy('sys_dict_data', 'dict_', 'dict_label', '起重机类型管理')
-        // 普通下拉框
-        this.dropdown = BindDropDownControls('customer', 'customer_id', 'username');
-    },
-    // 搜索下拉
-    bindsearchdropdown: function () {
-        BindDropDownControl('selectuserid', 'customer', 'customer_id', 'username')
-    },
-    // 添加页面
-    addPage: function () {
-        vm.reload();
-        vm.index = layer.open({
-            type: 1,
-            area: ['95%', '95%'],
-            content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
-             btn: ['确定', '取消'],
-             yes: function () {
-                 vm.saveOrUpdate();
-             },
-             btn2: function () {
-                 vm.reload();
-             }
-        });
-    },
-    <!--修改页面-->
-    editPage: function (x) {
-        var id = getSelectedRow();
-        if (id == null) {
-            return;
-        }
-        $.ajax({
-            url: baseURL + 'cMcurr/cMcurr/findByid',
-            type: "GET",
-            data: {
-        id: id,
-            },
-            success: function (result) {
-                                    vm.edit.id= result.data.id;
-                                    vm.edit.mname= result.data.mname;
-                                    vm.edit.teacherid= result.data.teacherid;
-                                    vm.edit.createtime= result.data.createtime;
-                                    vm.edit.isexamine= result.data.isexamine;
-                                    vm.edit.parentid= result.data.parentid;
-                                    vm.edit.remarks1= result.data.remarks1;
-                                    vm.edit.remarks2= result.data.remarks2;
-                                    vm.edit.remarks3= result.data.remarks3;
-                            }
-        })
-        vm.index = layer.open({
-            type: 1,
-            area: ['95%', '95%'],
-            content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
-             btn: ['确定', '取消'],
-             yes: function (id) {
-                 vm.saveOrUpdate(id);
-             },
-             btn2: function () {
-                 vm.reload();
-             }
-        });
-    },
-    <!--新增修改-->
-    saveOrUpdate: function (id) {
-        var url = id == null ? "cMcurr/cMcurr/adddata" : "cMcurr/cMcurr/updatedata";
-        $.ajax({
-            url: baseURL + url,
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(vm.edit),
-            success: function (result) {
-                vm.reload();
-                layer.close(vm.index)
-            },
-            btn2: function () {
-                vm.reload();
+            mname: null,
+            teacherid: null,
+            createtime: null,
+            isexamine: null,
+            parentid: null,
+            remarks1: null,
+            remarks2: null,
+            remarks3: null,
+        },
+        showList: true,
+        title: null,
+        index: null,
+        edit: {
+            id: null,
+            mname: null,
+            teacherid: null,
+            createtime: null,
+            isexamine: null,
+            parentid: null,
+            remarks1: null,
+            remarks2: null,
+            remarks3: null,
+        },
+        dropdown: [
+            {
+                id: null,
+                name: null,
             }
-        })
+        ],
+        dropdown1: [
+            {
+                id: null,
+                name: null,
+            }
+        ]
     },
-    // 搜索
-    query: function () {
-        var page = $("#jqGrid").jqGrid('getGridParam', 'page');
-        $("#jqGrid").jqGrid('setGridParam', {
-            postData: {
-                                                                                                'mname':vm.q.mname,
-                                                                                'teacherid':vm.q.teacherid,
-                                                                                'createtime':vm.q.createtime,
-                                                                                'isexamine':vm.q.isexamine,
-                                                                                'parentid':vm.q.parentid,
-                                                                                'remarks1':vm.q.remarks1,
-                                                                                'remarks2':vm.q.remarks2,
-                                                                                'remarks3':vm.q.remarks3,
-                                                },
-            page: page
-        }).trigger("reloadGrid");
+    created: function () {
+        this.bindsearchdropdown();
+        this.binddropdown();
     },
-    // 删除
-    del: function (event) {
-        var ids = getSelectedRows();
-        if (ids == null) {
-            return;
-        }
-        var r=layer.confirm('您确定要删除该用户吗?',{btn: ['确定', '取消'],title:"提示"}, function() {
-            if (r) {
-            $.ajax({
-                type: "POST",
-                url: baseURL + "/cMcurr/cMcurr/del",
-                contentType: "application/json",
-                data: JSON.stringify(ids),
-                success: function (r) {
-                    if (r.code = 1) {
-                        vm.reload();
-                    } else {
-                        alert("删除失败")
-                    }
-
+    methods: {
+        binddropdown: function () {
+            //字典表下拉框
+            this.dropdown1 = BindDropDownControlsdy('sys_dict_data', 'dict_', 'dict_label', '起重机类型管理')
+            // 普通下拉框
+            this.dropdown = BindDropDownControls('sys_dict_data', 'dict_type', 'dict_value');
+        },
+        // 搜索下拉
+        bindsearchdropdown: function () {
+            BindDropDownControl('sys_user_sex', 'sys_dict_data', 'dict_value', 'dict_value')
+        },
+        // 添加页面
+        addPage: function () {
+            vm.reload();
+            vm.index = layer.open({
+                type: 1,
+                area: ['95%', '95%'],
+                content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
+                btn: ['确定', '取消'],
+                yes: function () {
+                    vm.saveOrUpdate();
+                },
+                btn2: function () {
+                    vm.reload();
                 }
             });
+        },
+        <!--修改页面-->
+        editPage: function (x) {
+            var id = getSelectedRow();
+            if (id == null) {
+                return;
             }
-        });
-    },
-    // 重置
-    reset:function () {
-                                                        vm.q.mname=null;
-                                                vm.q.teacherid=null;
-                                                vm.q.createtime=null;
-                                                vm.q.isexamine=null;
-                                                vm.q.parentid=null;
-                                                vm.q.remarks1=null;
-                                                vm.q.remarks2=null;
-                                                vm.q.remarks3=null;
-                            $("#gen-form")[0].reset();
-    },
-    reload: function () {
-                                                        vm.edit.mname=null;
-                                                vm.edit.teacherid=null;
-                                                vm.edit.createtime=null;
-                                                vm.edit.isexamine=null;
-                                                vm.edit.parentid=null;
-                                                vm.edit.remarks1=null;
-                                                vm.edit.remarks2=null;
-                                                vm.edit.remarks3=null;
-                            $("#editLayer")[0].reset();
-        layer.closeAll();
-        $("#jqGrid").trigger("reloadGrid");
+            $.ajax({
+                url: baseURL + 'cMcurr/cMcurr/findByid',
+                type: "GET",
+                data: {
+                    id: id,
+                },
+                success: function (result) {
+                    vm.edit.id = result.data.id;
+                    vm.edit.mname = result.data.mname;
+                    vm.edit.teacherid = result.data.teacherid;
+                    vm.edit.createtime = result.data.createtime;
+                    vm.edit.isexamine = result.data.isexamine;
+                    vm.edit.parentid = result.data.parentid;
+                    vm.edit.remarks1 = result.data.remarks1;
+                    vm.edit.remarks2 = result.data.remarks2;
+                    vm.edit.remarks3 = result.data.remarks3;
+                }
+            })
+            vm.index = layer.open({
+                type: 1,
+                area: ['95%', '95%'],
+                content: $('#editLayer'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
+                btn: ['确定', '取消'],
+                yes: function (id) {
+                    vm.saveOrUpdate(id);
+                },
+                btn2: function () {
+                    vm.reload();
+                }
+            });
+        },
+        <!--新增修改-->
+        saveOrUpdate: function (id) {
+            var url = id == null ? "cMcurr/cMcurr/adddata" : "cMcurr/cMcurr/updatedata";
+            $.ajax({
+                url: baseURL + url,
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(vm.edit),
+                success: function (result) {
+                    vm.reload();
+                    layer.close(vm.index)
+                },
+                btn2: function () {
+                    vm.reload();
+                }
+            })
+        },
+        // 搜索
+        query: function () {
+            var page = $("#jqGrid").jqGrid('getGridParam', 'page');
+            $("#jqGrid").jqGrid('setGridParam', {
+                postData: {
+                    'mname': vm.q.mname,
+                    'teacherid': vm.q.teacherid,
+                    'createtime': vm.q.createtime,
+                    'isexamine': vm.q.isexamine,
+                    'parentid': vm.q.parentid,
+                    'remarks1': vm.q.remarks1,
+                    'remarks2': vm.q.remarks2,
+                    'remarks3': vm.q.remarks3,
+                },
+                page: page
+            }).trigger("reloadGrid");
+        },
+        // 删除
+        del: function (event) {
+            var ids = getSelectedRows();
+            if (ids == null) {
+                return;
+            }
+            var r = layer.confirm('您确定要删除该用户吗?', {btn: ['确定', '取消'], title: "提示"}, function () {
+                if (r) {
+                    $.ajax({
+                        type: "POST",
+                        url: baseURL + "/cMcurr/cMcurr/del",
+                        contentType: "application/json",
+                        data: JSON.stringify(ids),
+                        success: function (r) {
+                            if (r.code = 1) {
+                                vm.reload();
+                            } else {
+                                alert("删除失败")
+                            }
+
+                        }
+                    });
+                }
+            });
+        },
+        // 重置
+        reset: function () {
+            vm.q.mname = null;
+            vm.q.teacherid = null;
+            vm.q.createtime = null;
+            vm.q.isexamine = null;
+            vm.q.parentid = null;
+            vm.q.remarks1 = null;
+            vm.q.remarks2 = null;
+            vm.q.remarks3 = null;
+            $("#gen-form")[0].reset();
+        },
+        reload: function () {
+            vm.edit.mname = null;
+            vm.edit.teacherid = null;
+            vm.edit.createtime = null;
+            vm.edit.isexamine = null;
+            vm.edit.parentid = null;
+            vm.edit.remarks1 = null;
+            vm.edit.remarks2 = null;
+            vm.edit.remarks3 = null;
+            $("#editLayer")[0].reset();
+            layer.closeAll();
+            $("#jqGrid").trigger("reloadGrid");
+        }
     }
-}
 });
